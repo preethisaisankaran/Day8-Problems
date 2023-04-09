115d1e2d90a71fba8e30c93f1df2ae2d4d35c3b4
preethisaisankaran <preethisankaran11@gmail.com>
2023-04-09 20:42:35 +0530

    ADDED:Calculate wages

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,7 +3,9 @@ import java.util.Random;
 public class EmployeeWage {
     public static void main(String[] args){
         System.out.println("Welcome to Employee Wage Computation");
-        Employee emp1=new Employee("Preethi",1001);
+        Employee emp1=new Employee("Preethi",1001,8);
         emp1.markAttendance();
+        int wage=emp1.calculateWage();
+        System.out.println("earned Rs"+wage+"today.");
     }
 }
