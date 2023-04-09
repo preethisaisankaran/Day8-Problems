e475aea9ebb07766e156ad3013377cf9569e3526
preethisaisankaran <preethisankaran11@gmail.com>
2023-04-09 19:40:48 +0530

    ADDED:Mark Attendance

---
+++ Employee.java
@@ -0,0 +1,2 @@
+package com.wages;public class Sample1 {
+}
--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,7 +1,9 @@
 package com.wages;
-
+import java.util.Random;
 public class EmployeeWage {
     public static void main(String[] args){
         System.out.println("Welcome to Employee Wage Computation");
+        Employee emp1=new Employee("Preethi",1001);
+        emp1.markAttendance();
     }
 }
